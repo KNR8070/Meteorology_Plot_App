6824f96ce149d91870b0a2f95257b6cdd8744fa7
narender reddy <asz198070@iitd.ac.in>
2024-11-13 14:35:02 +0530

    chnaging alpha of quiver plot

--- app_all_variables.py
+++ app_all_variables.py
@@ -109,7 +109,7 @@ def plot_wind_vectors(ds_u,ds_v, lat_min, lat_max, lon_min, lon_max, time_s):
     ax.quiver(lons[::alt_num,::alt_num], 
               lats[::alt_num,::alt_num], 
               ds_u[::alt_num,::alt_num], 
-              ds_v[::alt_num,::alt_num])
+              ds_v[::alt_num,::alt_num],alpha=0.6)
     # if lon_max>180:
     #     ax.set_xticks(np.linspace(lon_min-180,lon_max-180,num=5,endpoint=True))
     # else:
