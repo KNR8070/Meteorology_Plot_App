dc142fb5b5559e3132ff1c6c22cfb82f5360d738
narender reddy <asz198070@iitd.ac.in>
2024-12-30 17:41:58 +0530

    minor bug

--- app_all_variables.py
+++ app_all_variables.py
@@ -172,7 +172,7 @@ def plot_spatial2(var_subset,lat_min, lat_max, lon_min, lon_max,time_s):
         s_plot = ax3.contourf(lons,lats,plot_data,
                               cmap=cmc.vik, 
                               vmin=-30, vamx=30,
-                              levels=np.linspace(-30,30,31)
+                              levels=np.linspace(-30,30,31),
                               extend='both')
         if x_size<y_size:
             cbar = fig.colorbar(s_plot, ax=ax3,shrink=0.3)# label="2m Temperature (degC)",                  
